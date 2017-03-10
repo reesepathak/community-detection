f4f6e791458a63e397778cba1c1bd5128fefea3e
Ben Pastel <benpastel@gmail.com>
2017-03-10 13:34:31 -0800

    clean up some mess from yesterday's first pass.  sign_stats still not symmetric.

--- agnostic_sphere_comparison.py
+++ agnostic_sphere_comparison.py
@@ -3,16 +3,14 @@ import numpy as np
 from math import log, ceil
 
 random.seed(1)
-n = 1490 # this is the total number of nodes in the dataset, including 0-degree nodes
 
-# load blog data
-# TODO: come back and reason about the size of these nodes.
-# in particular, the original paper says 1494 and we have 1490
+# the total number of nodes in the dataset, including 0-degree nodes
+n = 1490 
+# TODO: why are there 1490 nodes? the original paper says 1494
 # they also talk about filtering them
 # AND the final community numbers don't add up properly
-#
-# TODO: should we ceil or floor r?
-#
+
+# load blog data
 with open('data/polblogs.gml', 'r') as f:
     raw = f.readlines()
 
@@ -23,50 +21,17 @@ for r, line in enumerate(raw):
         v2 = int(raw[r+2].strip().split()[1])
         if v1 == v2: continue
         v1, v2 = (v1 - 1, v2 - 1) # shift to 0-indexing
-
         e = (v1, v2) if v1 < v2 else (v2, v1)
         edges.add(e)
-adj = np.zeros(n, dtype=object)
-for i in range(n):
-    adj[i] = set()
+d = 2.0 * len(edges) / n
 
-for (v1, v2) in edges:
-    adj[v1].add(v2)
-    adj[v2].add(v1)
+global_r = ceil(3.0 / 4.0 * log(n) / log(d)) # ASSUME r = r' 
+# TODO: should this be ceil or floor?
 
-d = 2.0 * len(edges) / len(nodes)
-r_global = 3.0 / 4.0 * log(n) / log(d) # ASSUME r = r' 
-c = 0.1
-
-E = {e for e in edges if random.random() < c}
-g_minus_e = edges - E
-
-import random 
-import numpy as np
-from math import log
-random.seed(1)
-n = 1490 # this is the total number of nodes in the dataset, including 0-degree nodes
-# load blog data
-# TODO: come back and reason about the size of these nodes.
-# in particular, the original paper says 1494 and we have 1490
-# they also talk about filtering them
-# AND the final community numbers don't add up properly
-with open('data/polblogs.gml', 'r') as f:
-    raw = f.readlines()
-edges = set()
-for r, line in enumerate(raw):
-    if ' edge ' in line:
-        v1 = int(raw[r+1].strip().split()[1]) - 1
-        v2 = int(raw[r+2].strip().split()[1])
-        if v1 == v2: continue
-        v1, v2 = (v1 - 1, v2 - 1) # shift to 0-indexing
-        e = (v1, v2) if v1 < v2 else (v2, v1)
-        edges.add(e)
-d = 2.0 * len(edges) / len(nodes)
-global_r = 3.0 / 4.0 * log(n) / log(d) # ASSUME r = r' 
 c = 0.1
 E = {e for e in edges if random.random() < c}
 g_minus_e = edges - E
+
 adj = np.zeros(n, dtype=object)
 for i in range(n):
     adj[i] = set()
@@ -76,22 +41,19 @@ for (v1, v2) in g_minus_e:
 
 def nbors(r, v):
     """nbors[r, v] := his N_{r[G\E]}(v)"""
-    depth = [0 for i in range(n)]
-    visited = [False for i in range(n)]
+    depth = np.zeros(n, dtype=int)
+    visited = np.zeros(n, dtype=bool)
     bfs_queue = [v]
     visited[v] = True
     while len(bfs_queue) > 0:
         front = bfs_queue.pop()
-        for node in adj[front]:
-            if not visited[node]:
-                depth[node] = depth[front]+1
-                visited[node] = True
-                bfs_queue.append(node)
-    ret = set()
-    for node in range(n):
-        if depth[node] == ceil(r): 
-            ret.add(node)
-    return ret
+        for nbor in adj[front]:
+            if not visited[nbor]:
+                depth[nbor] = depth[front]+1
+                visited[nbor] = True
+                bfs_queue.append(nbor)
+    return [v for v in range(n) if depth[v] == r]
+
 def nbor_mutuals(r, r_, v, v_):
     """nbor_mutuals[r, v, v2] := his N_{r, r' [E]}(v, v')"""
     nbor_rv = nbors(r, v)
@@ -101,25 +63,24 @@ def nbor_mutuals(r, r_, v, v_):
     for v1 in nbor_rv:
         for v2 in nbor_r_v_:
             if (v1, v2) in E or (v2, v1) in E:
-                count = count+1
+                count +=1
     return count
+
 def sign_stat(v, v_, r = global_r, r_ = global_r):
     """sign_stat[v, v2] := his I_{global_r, global_r, [E]}(v, v')"""
     x = nbor_mutuals(r, r_, v, v_)
     y = nbor_mutuals(r+1, r_, v, v_)
     z = nbor_mutuals(r+2, r_, v, v_)
-    return x*z-y*y
+    return x*z - y*y
 
-# select 5 vertices randomly [v1, v2, ..., v5]
-# for each pair (v_i, v_j)
-#   compute sign_stat(r, r, v_i, v_j)
-#
+
 # try to find a partition such that sign_stat(v_i, v_j) > 0 
 #   iff v_i and v_j are in the same community
 #   if we can't, fail
 #   if we can, pick a random node from each community to be our anchor
 #
-# for each other node w, assign it to maximize sign_stat(anchor(community), w)
+
+# select 5 vertices randomly and compute pairwise sign statistics
 v = random.sample(xrange(n), 5)
 sign_stats = np.zeros((len(v), len(v)))
 for i in range(len(v)):
@@ -128,6 +89,8 @@ for i in range(len(v)):
 
 print "sign_stats", sign_stats
 
+# check if there exists a consistent partition of these vertices
+#   i.e. sign_stat[v_i, v_j] > 0 iff v_i and v_j are in the same community
 for i in range(2**len(v)):
     split1 = []
     split2 = []
@@ -160,10 +123,11 @@ for i in range(2**len(v)):
         anchor1 = split1[0]
         anchor2 = split2[0]
         break
-    # else:
-    #     print "split1:", split1
-    #     print "split2:", split2
+
 if not partition:
     raise Exception("couldn't find partition")
+
+# TODO: for each other node w, assign it to maximize sign_stat(anchor(community), w)
+
 
 
